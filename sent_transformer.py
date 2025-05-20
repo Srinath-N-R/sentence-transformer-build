3f4bf234bdc3322bfc434ab9a1f6c920d3c41bfd
Srinath N Ramalingam <61198500+Srinath-N-R@users.noreply.github.com>
2025-05-20 12:28:30 -0400

    first

--- sent_transformer.py
+++ sent_transformer.py
@@ -24,6 +24,7 @@ class SentenceTransformer(nn.Module):
         self.proj_dropout = nn.Dropout(dropout)
         self.proj = nn.Linear(d_model, proj_dim)
         self.proj_norm = nn.LayerNorm(proj_dim)
+        self.tanh = nn.Tanh()
 
     def forward(self, x, attention_mask=None, return_all=False):
         if attention_mask is None:
@@ -38,7 +39,7 @@ class SentenceTransformer(nn.Module):
         
         cls = h[:, 0] # CLS pooling (position 0 token, CLS_ID)
 
-        sent_emb = self.proj_norm(self.proj(self.proj_dropout(cls))) # projection head for sentence tasks
+        sent_emb = self.tanh(self.proj_norm(self.proj(self.proj_dropout(cls)))) # projection head for sentence tasks
 
         if return_all:
             return sent_emb, h
